292b157cc5f2ae77d8d19196c8c35f2e7afb0914
lincoln <maestaslincoln1@gmail.com>
2020-10-22 11:58:02 -0600

    Main Push

---
+++ RNGRecovertest/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace RNGRecovertest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(425, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Calculate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Recover);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 73);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(488, 267);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TID:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "PID:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox2.Location = new System.Drawing.Point(98, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(81, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "0x";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Initial";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Frame";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "TID";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 40;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "SID";
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 40;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Shiny";
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 60;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Date";
+            this.Column6.Name = "Column6";
+            this.Column6.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 352);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "RS TID/SID Frame Finder";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ RNGRecovertest/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RNGRecovertest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Recover(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            uint add;
+            uint k;
+            uint mult;
+            byte[] low = new byte[0x10000];
+            bool[] flags = new bool[0x10000];
+            
+            k = 0xC64E6D00; // Mult << 8
+            mult = 0x41c64e6d; // pokerng constant
+            add = 0x6073; // pokerng constant
+            uint count = 0;
+            foreach (byte element in low) {
+                low[count] = 0;
+                count++;
+                    }
+            count = 0;
+            foreach (bool element in flags)
+            {
+                flags[count] = true;
+                count++;
+            }
+            for (short i = 0; i < 256; i++)
+            {
+                uint right = (uint)(mult * i + add);
+                ushort val = (ushort)(right >> 16);
+                flags[val] = true;
+                low[val--] = (byte)(i);
+                flags[val] = true;
+                low[val] = (byte)(i);
+            }
+            uint tid = uint.Parse(textBox1.Text);
+            string pid = textBox2.Text;
+            uint PIDhigh = uint.Parse(pid.Substring(0, 4), System.Globalization.NumberStyles.HexNumber);
+            uint PIDlow = uint.Parse(pid.Substring(4, 4), System.Globalization.NumberStyles.HexNumber);
+            uint psv = (PIDlow ^ PIDhigh) / 8;
+            uint prv = (PIDlow ^ PIDhigh) - 8 * psv;
+            List<uint> sids = new List<uint>();
+            List<uint> sids2 = new List<uint>();
+            for (uint testsid = 0; testsid < 0xFFFF; testsid++)
+            {
+                if ((testsid ^ tid)/8 == psv)
+                {
+                    sids.Add(testsid);
+                }
+            }
+            List<uint> origin = new List<uint>();
+            foreach (uint sid in sids)
+            {
+                string TSIDlow = sid.ToString("X4");
+                string TSIDhigh = tid.ToString("X4");
+                string TSID = TSIDhigh + TSIDlow;
+                
+                uint tspid = uint.Parse(TSID, System.Globalization.NumberStyles.HexNumber);
+                uint first = tspid << 16;
+                uint second = (uint)(tspid & 0xFFFF0000);
+                uint search = (uint)(second - first * mult);
+                for (short i = 0; i < 256; i++, search -= k)
+                {
+                    if (flags[search >> 16])
+                    {
+                        uint test = first | (uint)(i << 8) | low[search >> 16];
+                        if (((test * mult + add) & 0xffff0000) == second)
+                        {
+                            LCRNG rng = new LCRNG((int)test);
+                            int seed = rng.nextUInt();
+                            origin.Add((uint)seed);
+                            sids2.Add(sid);
+                        }
+                    }
+                }
+            }
+            uint frame;
+            int index = 0;
+            foreach (int s in origin)
+            {
+                frame = 1;
+                LCRNG rng = new LCRNG(s);
+                while ((uint)rng.seed > (uint)0xFFFF)
+                {
+                    rng.nextUInt();
+                    frame++;
+                }
+                uint temptsv = (sids2[index] ^ tid)/8;
+                uint temptrv = sids2[index] ^ tid - 8 * temptsv;
+                string shiny = "Star";
+                if (temptrv == prv)
+                {
+                    shiny = "Square";
+                }
+                int ugh = sids2.Count;
+                var start = new DateTime(2000, 1, 1, 1, 0, 0);
+                start = DateTime.SpecifyKind(start, DateTimeKind.Utc);
+                var lastdate = new DateTime(start.Year, 12, 31);
+                lastdate = DateTime.SpecifyKind(lastdate, DateTimeKind.Utc);
+                int minDay = 0;
+                int maxDay = 0;
+                List<DateTime> times = new List<DateTime>();
+                for (short x = 2001; x < 2000; x++)
+                {
+                    var temp = new DateTime(x, 1, 1);
+                    temp = DateTime.SpecifyKind(temp, DateTimeKind.Utc);
+                    minDay += (int)(lastdate - start).TotalDays;
+                    maxDay += (int)(lastdate - start).TotalDays;
+                }
+                for (byte month = 1; month < 13; month++)
+                {
+                    var temp = new DateTime(2000, month, 1);
+                    maxDay += DateTime.DaysInMonth(2000, month);
+                    for (int day = minDay; day < maxDay; day++)
+                    {
+                        for (int hour = 0; hour < 24; hour++)
+                        {
+                            for (int minute = 0; minute < 60; minute++)
+                            {
+                                int v = 1440 * day + 960 * (hour / 10) + 60 * (hour % 10) + 16 * (minute / 10) + (minute % 10) + 0x5A0;
+                                v = (v >> 16) ^ (v & 0xFFFF);
+                                if (v == rng.seed)
+                                {
+                                    var finalTime = start.AddDays(day).AddSeconds(((hour-1) * 60 * 60) + (minute * 60));
+                                    finalTime = DateTime.SpecifyKind(finalTime, DateTimeKind.Utc);
+                                    times.Add(finalTime);
+                                    
+
+                                }
+                            }
+                        }
+                    }
+                    minDay += DateTime.DaysInMonth(2000, month);
+                }
+                times.Sort((a, b) => a.Month.CompareTo(b.Month));
+                var dt = DateTime.SpecifyKind(times[0], DateTimeKind.Utc);
+                string timestring = dt.ToString("ddd MMM d HH:mm:ss yyyy");
+
+                dataGridView1.Rows.Add(rng.seed.ToString("X4"), frame.ToString(),tid,sids2[index++],shiny,timestring);
+
+            }
+        }
+    }
+}
